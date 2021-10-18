f376da0db395b2dff6c69eb7c73db31247f8206e
pengnanfang <pengnanfang@bytedance.com>
2021-10-18 18:21:52 +0800

    docker java-spring demo

---
+++ java-spring/src/main/java/com/lydia/demo/HelloController.java
@@ -0,0 +1,17 @@
+package com.lydia.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
